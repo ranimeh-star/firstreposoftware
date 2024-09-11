524d0b290914dc5f917bcd52462e1c47d6d61d0b
ranimeh-star <ranimeh@sheridancollega.ca>
2024-09-11 13:19:41 -0500

    My first project is here

--- src/myfirstsoftpro/Myfirstsoftpro.java
+++ src/myfirstsoftpro/Myfirstsoftpro.java
@@ -14,7 +14,7 @@ public class Myfirstsoftpro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
         // TODO code application logic here
     }
     
